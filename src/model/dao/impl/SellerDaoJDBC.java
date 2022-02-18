1706baa9245d357b5ea4fc336eae8c9cd0f3a6f3
rafaelofficial <rafael.sousa.pereira@mail.usf.edu.br>
2022-02-18 15:17:03 -0300

    insert implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -25,8 +26,43 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public void insert(Seller obj) {
-		// TODO Auto-generated method stub
-		
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement(
+					"INSERT INTO seller\r\n"
+					+ "		(Name, Email, BirthDate, BaseSalary, DepartmentId) \r\n"
+					+ "		VALUES \r\n"
+					+ "		(?, ?, ?, ?, ?)",
+					Statement.RETURN_GENERATED_KEYS);
+			
+			// configurar os placeholder
+			st.setString(1, obj.getName());
+			st.setString(2, obj.getEmail());
+			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+			st.setDouble(4, obj.getBaseSalary());
+			st.setInt(5, obj.getDepartment().getId());
+			
+			// executa o comando sql
+			int rowsAffected = st.executeUpdate();
+			
+			// testar se uma ou mais linhas foram alteradas
+			if (rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				if (rs.next()) {
+					int id = rs.getInt(1);
+					obj.setId(id);
+				} 
+				DB.closeResultSet(rs);
+			} else {
+				throw new DbException("Unexpected error! No rows affected!");
+			}
+			
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+		}	
 	}
 
 	@Override
@@ -57,7 +93,7 @@ public class SellerDaoJDBC implements SellerDao {
 					+ "ON seller.DepartmentId = department.Id " 
 					+ "WHERE seller.Id = ?");
 			
-			// configurar o ? da busca no banco de dados
+			// configurar o placeholder da busca no banco de dados
 			st.setInt(1, id);
 			rs = st.executeQuery();
 			
@@ -111,7 +147,7 @@ public class SellerDaoJDBC implements SellerDao {
 					+ "ON seller.DepartmentId = department.Id "
 					+ "ORDER BY Name");
 			
-			// configurar o ? da busca no banco de dados
+			// configurar o placeholder da busca no banco de dados
 			rs = st.executeQuery();
 			
 			List<Seller> list = new ArrayList<>();
